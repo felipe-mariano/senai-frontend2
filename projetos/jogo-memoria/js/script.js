c3dc2ef09985a5223cf320d23c8743a4df9f82b1
Felipe <felipe.cnbsb@gmail.com>
2024-05-13 20:08:38 -0300

    init: script do jogo da memória

---
+++ projetos/jogo-memoria/js/script.js
@@ -0,0 +1,8 @@
+//Lista dos itens que estarão no jogo
+let lsEmoticons = ["😊", "😂", "😍", "😎", "🤔", "🤦‍♂️", "🐱‍👤", "😜",]
+
+// replicar os itens na tabela
+const tamanho = lsEmoticons.length;
+for (let i = 0; i < tamanho; i++) {
+    lsEmoticons.push(lsEmoticons[i])
+}
