6c62b45810c51f3891dd3a9283671d8a30888833
Felipe <felipe.cnbsb@gmail.com>
2024-05-13 21:40:58 -0300

    update: funcionalidade para verificar os pares

--- projetos/jogo-memoria/js/script.js
+++ projetos/jogo-memoria/js/script.js
@@ -15,15 +15,39 @@ for (const b of lsBloco) {
     b.addEventListener("click", () => {
         mostrar(b);
     });
-    icon = lsEmoticons.slice(sortear(), 1);
+    icon = lsEmoticons.splice(sortear(), 1);
     blocos[`id${id}`] = icon;
     b.id = `id${id}`;
     id++;
 }
 
+let aberto = ["", ""];
 function mostrar(b) {
-    b.innerHTML = blocos[b.id];
+    if (b.innerHTML != "") return;
+
+    if (aberto[0] !="" && aberto[1] != "") testar();
+
+    if (aberto[0] == "") {
+        aberto[0] = b;
+        b.innerHTML = blocos[b.id];
+        return;
+    }
+
+    if (aberto[1] == "") {
+        aberto[1] = b;
+        b.innerHTML = blocos[b.id];
+        return;
+    }
+    function testar() {
+        if (aberto[0].innerHTML != aberto[1].innerHTML) {
+            aberto[0].innerHTML = "";
+            aberto[1].innerHTML = "";
+        }
+        aberto[0] = "";
+        aberto[1] = "";
+    }
 }
+
 
 function sortear() {
     return Math.floor(Math.random() * lsEmoticons.length);
