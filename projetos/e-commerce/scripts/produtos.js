3365396e2f1387da9d0b268f3713fa0a1a0643a2
Felipe <felipe.cnbsb@gmail.com>
2024-06-10 20:24:24 -0300

    Atualizando o menu inicial, adicionando o botão carrinho

---
+++ projetos/e-commerce/scripts/produtos.js
@@ -0,0 +1,37 @@
+let produtos = [
+    {
+        img: "img/vela01.jpg"
+        dsImg: "Vela 1",
+        nome: "Vela Alecrim - 100G",
+        valor: 35.00
+    },
+{
+    img: "img/vela02.jpg",
+    dsImg: "Vela 2",
+    nome: "Vela Amêndoa Vegana- 100G",
+    valor: 55.00   
+},
+{
+    img: "img/vela03.jpg",
+    dsImg: "Vela 3",
+    nome: "Kit com 03 velas vegetais - 300G",
+    valor: 59.69   
+},
+{
+    img: "img/vela04.jpeg",
+    dsImg: "Vela 4",
+    nome: "Vela Sândalo - 200G",
+    valor: 125.48
+},
+{
+    img: "img/vela05.jpg",
+    dsImg: "Vela 5",
+    nome: "Vela Cidreira - 100G",
+    valor: 85.00   
+},
+{
+    img: "img/vela06.jpg",
+    dsImg: "Vela 6",
+    nome: "Vela de Citronela - 200G",
+    valor: 14.99    
+}]
--- projetos/e-commerce/scripts/scripts.js
+++ projetos/e-commerce/scripts/scripts.js
@@ -1,44 +1,58 @@
-let menuContent = document.querySelector('.content');
+let menuContent = document.querySelector('.home');
 let menuToggle = menuContent.querySelector('.menu-toggle');
 let show = true;
+
 menuToggle.addEventListener('click', () =>{
     document.body.style.overflow = show ? 'hidden' : 'initial';
     menuContent.classList.toggle('on', show);
     show = !show;
 })
-let itensLançamento = document.querySelector(".itens-lancamento");
+
+let itensProdutos = document.querySelector(".itens-produtos");
 let id = 0;
 for (const p of produtos) {
-    itensLancamento.innerHTML += `
-    <div class="">
+    itensProdutos.innerHTML += `
+    <div class="item">
         <img src="${p.img}" alt="${p.dsImg}">
         <div class="info">
             <h3>${p.nome}</h3>
-            <h4>unidade<span>R$${p.sapato}</span></h4>
+            <h4><span>R$${p.valor.toFixed(2)}</span></h4>
             <button id="id${id}" class="pedir">pedir agora</button>
         </div>
     </div>
     `;
     id++;
 }
+
 let telaCarrinho = document.querySelector('.tela-carrinho');
 let continuar = document.querySelector('.continuar');
-continuar.addEventListener('click',()=>{
+continuar.addEventListener('click',() => {
     telaCarrinho.classList.toggle('ocultar-tela-carrinho');
+    menuToggle.click(); 
 })
+
 let compras = document.querySelector('.compras');
-compras.addEventListener("click",()=>{
+compras.addEventListener("click",() => {
     telaCarrinho.classList.toggle('ocultar-tela-carrinho');
     menuToggle.click(); // fechar menu versão mobile
 } )
+
 let lsPedido = document.querySelectorAll('.pedir');
 for (const bt of lsPedido) {
-    bt.addEventListener('click', ()=>{
+    bt.addEventListener('click', () => {
         let id = bt.id.replace('id','');
-        produtos[id].quantidade = 1;
+        bt.classList.toggle('selecionado');
+        if(bt.innerHTML == 'REMOVER'){
+            produtos[id].quantidade = 0;
+            bt.innerHTML = 'pedir agora';
+        } else{
+            produtos[id].quantidade = 1;
+            bt.innerHTML = 'REMOVER';
+        }
         atualizarTabela();
     });
 }
+
 let tbody = document.querySelector('tbody');
 function atualizarTabela() {
     tbody.innerHTML = '';
@@ -49,53 +63,59 @@ function atualizarTabela() {
             tbody.innerHTML += `
                         <tr>
                         <td>${p.nome}</td>
-                        <td>${p.quantidade}x${p.velasAromaticas}=${p.quantidade*p.velasAromaticas}(unidade)</td>
+                        <td>${p.quantidade}x${p.valor}=${p.quantidade*p.valor}</td>
                         <td>
                             <i class="bi bi-plus-square-fill" id="plus${id}"></i>
                             <i class="bi bi-dash-square-fill" id="dash${id}"></i>
                         </td>
                     </tr>`;
-        total += p.quantidade*p.velasAromaticas;
+        total += p.quantidade*p.valor;
             
         }
         id++;
     }
+
     document.querySelector('#total-pedido').innerHTML =`Valor total do pedido = R$${total}`;
     atualizarPlusDash('plus');
     atualizarPlusDash('dash');
 }
+
 function atualizarPlusDash(tipo){
     let botoes = document.querySelectorAll(`.bi-${tipo}-square-fill`);
     for (const bt of botoes) {
-        bt.addEventListener('click', () =>{
+        bt.addEventListener('click', () => {
             let id = bt.id.replace(tipo, '');
             if(tipo == 'plus'){
                 produtos[id].quantidade ++;               
             }
             if(tipo == 'dash'){
                 produtos[id].quantidade --;
+                if(produtos[id].quantidade < 1){
+                    document.getElementById('id' +id).click();
+                }
             }
             atualizarTabela();
-        })
+        });
         
     }
 
 }
 
 let enviar = document.querySelector('.enviar');
-enviar.addEventListener("click", ()=>{
-    let msg = 'Gostaria de fazer o seguinte pedido\n';
+enviar.addEventListener("click", () => {
+    let msg = 'Gostaria de fazer o pedido\n';
     let total = 0;
        for (const p of produtos) {
         if (p.quantidade > 0) {
-            msg += `${p.nome} ${p.quantidade}x${p.velasAromaticas}=${p.quantidade*p.velasAromaticas}\n`;
-        total += p.quantidade * p.velasAromaticas;
+            msg += `${p.nome} ${p.quantidade}x${p.valor}=${p.quantidade * p.valor}\n`;
+        total += p.quantidade * p.valor;
 
         }
       }
+
       msg += `Total = ${total}`;
       msg = encodeURI(msg);
-      let fone = '5561997777070';
+      let fone = '5561981763599';
       let link = `https://api.whatsapp.com/send?phone=${fone}&text=${msg}`;
       window.open(link);
 
