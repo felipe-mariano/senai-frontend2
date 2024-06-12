9a97bf52b073ae25ee09ba709698dfe310a18ac4
Felipe <felipe.cnbsb@gmail.com>
2024-06-11 21:15:07 -0300

    colocando imagem de fundo e finalizando o projeto.

--- projetos/e-commerce/scripts/produtos.js
+++ projetos/e-commerce/scripts/produtos.js
@@ -1,37 +1,57 @@
 let produtos = [
-    {
-        img: "projetos/e-commerce/img/vela01.jpg"
-        dsImg: "Vela Alecrim",
-        nome: "Vela Alecrim - 100G",
-        valor: 35.00
-    },
+    
 {
-    img: "projetos/e-commerce/img/vela02.jpg",
+    img: "imagens/vela02.jpg",
     dsImg: "Vela Amêndoa",
     nome: "Vela Amêndoa Vegana- 100G",
-    valor: 55.00   
+    valor: 55.90   
 },
 {
-    img: "projetos/e-commerce/img/vela03.jpg",
+    img: "imagens/vela03.jpg",
     dsImg: "Kit Vela",
     nome: "Kit com 03 velas vegetais - 300G",
-    valor: 59.69   
+    valor: 129.99   
 },
 {
-    img: "projetos/e-commerce/img/vela04.jpg",
+    img: "imagens/vela04.jpg",
     dsImg: "Vela Sândalo",
     nome: "Vela Sândalo - 200G",
     valor: 125.48
 },
 {
-    img: "projetos/e-commerce/img/vela05.jpg",
+    img: "imagens/vela05.jpg",
     dsImg: "Vela Cidreira",
-    nome: "Vela Cidreira - 100G",
+    nome: "Vela Cidreira - 150G",
     valor: 85.00   
 },
 {
-    img: "projetos/e-commerce/img/vela06.jpg",
+    img: "imagens/vela06.jpg",
     dsImg: "Vela Citronela",
     nome: "Vela de Citronela - 200G",
-    valor: 14.99    
-}]
+    valor: 49.99    
+},
+{
+    img: "imagens/vela01.jpg",
+    dsImg: "Vela Alecrim",
+    nome: "Vela Alecrim - 150G",
+    valor: 64.99    
+},
+{
+    img: "imagens/vela07.jpg",
+    dsImg: "Vela Bothânica Vegana",
+    nome: "Vela Bothânica Vegana - 150G",
+    valor: 79.99
+},
+{
+    img: "imagens/vela08.jpg",
+    dsImg: "Vela Limão Siciliano",
+    nome: "Vela Limão Siciliano - 150G",
+    valor: 85.99
+},
+{
+    img: "imagens/vela09.jpg",
+    dsImg: "Vela Baunilha Vanilla",
+    nome: "Vela Baunilha Vanilla - 250G",
+    valor: 55.99
+},
+]
